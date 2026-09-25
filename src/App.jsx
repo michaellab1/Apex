c3632e8a6f7ec43d9fb7a6e56efc843eccfa8d33
unknown <michaeleboe4@gmail.com>
2026-09-25 11:15:19 +0100

    add SystemMetrics component

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Header from "./component/Header";
 import Hero from "./component/Hero";
 import BentoGrid from "./component/BentoGrid";
 import Codeshowcase from "./component/Codeshowcase";
-
+import SystemMetrics from "./component/SystemMetrics";
 function App() {
   return (
     <div className="min-h-screen flex flex-col items-center justify-between bg-[#090d16] text-slate-100 antialiased selection:bg-slate-200 selection:bg-indigo-500 selection:text-white">
@@ -11,6 +11,7 @@ function App() {
         <Hero />
         <BentoGrid />
         <Codeshowcase />
+        <SystemMetrics />
       </main>
     </div>
   );
